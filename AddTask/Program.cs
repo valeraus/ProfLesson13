730fb56e33f214b2264021ecbf5b300c7c6a1be8
Valera <vlaus0@gmail.com>
2023-04-26 22:13:10 +0300

    Добавьте файлы проекта.

---
+++ AddTask/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+    /*
+    Створіть масив чисел розмірністю в 1 000 000 або більше. Використовуючи генератор випадкових чисел, проініціалізуйте цей масив значеннями. 
+    Створіть запит PLINQ, який дозволить отримати всі непарні числа з цього масиву.
+     */
+    internal class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            int[] array = new int[1000000];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next();
+            }
+
+            var query = array.AsParallel().Where((i) => i % 2 != 0).Select((i) => i);
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    /*
+    Створіть два методи, які будуть виконуватися в рамках паралельних завдань. 
+    Організуйте виклик цих методів за допомогою Invoke таким чином, щоб основний потік програми (метод Main) не зупинив своє виконання.
+     */
+    internal class Program
+    {
+        static void Method1()
+        {
+            Console.WriteLine("Method1() запущен.");
+            for (int count = 0; count < 5; count++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("В методе Method1(), счетчик равен: " + count);
+            }
+            Console.WriteLine("Method1() завершен.");
+        }
+
+        static void Method2()
+        {
+            Console.WriteLine("Method2() запущен.");
+            for (int count = 0; count < 5; count++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("В методе Method2(), счетчик равен: " + count);
+            }
+            Console.WriteLine("Method2() завершен.");
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Основной поток запущен.");
+
+            Task.Factory.StartNew(() => Parallel.Invoke(Method1, Method2));
+
+            Console.WriteLine("Основной поток завершен.");
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
